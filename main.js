360d9c79fc571666a7b506b7a5961527a122670d
Fazlul Fazal <rahuman@gmail.com>
2019-03-19 11:19:09 +1100

    first commit

--- main.js
+++ main.js
@@ -1,6 +1,2 @@
 var num = 50;
-<<<<<<< HEAD
-var truth = "Hello Flatter Earth";
-=======
-var truth = "Hello Flat Earth";
->>>>>>> 20e95230bb151ebf0f00a3ddc72152a7cdb1543a
+var truth = "Hello World";
